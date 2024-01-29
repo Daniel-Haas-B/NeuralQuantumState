18de58ea3af4c89ea1a286f98cc196a150d00c2b
Daniel <daniel.lima@inmetrics.com.br>
2024-01-29 13:07:05 +0100

    batch incomplete

--- src/simulation_scripts/vmc_playground.py
+++ src/simulation_scripts/vmc_playground.py
@@ -20,13 +20,13 @@ nparticles = 2
 dim = 2
 nsamples = int(2**18)  # 2**18 = 262144
 nchains = 4
-eta = 0.01
+eta = 0.1
 
-training_cycles = 50_000  # this is cycles for the ansatz
+training_cycles = 250  # this is cycles for the ansatz
 mcmc_alg = "m"
 backend = "jax"
-optimizer = "sr"
-batch_size = 100
+optimizer = "adam"
+batch_size = 200
 detailed = True
 wf_type = "vmc"
 seed = 142
